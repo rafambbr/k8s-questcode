691d63aef2d47ac7a30475d80eed21accf3f2242
Rafael Camargo <rafael.camargo.sp@gmail.com>
2019-08-07 17:33:30 -0300

    fix

--- 06-questcode/front_back.sh
+++ 06-questcode/front_back.sh
@@ -3,3 +3,5 @@ kubectl apply -f 02-frontend-service.yaml -n staging
 
 kubectl apply -f 03-backend-user-deploy.yaml -n staging
 kubectl apply -f 03-backend-user-service.yaml -n staging
+
+kubectl get all -n staging
